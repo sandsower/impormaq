f8a4b6ea7468af3782896166722e1fe7f32c7019
Ben Sanders <bensandsower@gmail.com>
2011-10-23 22:06:31 -0500

    Terminado manejador de contenido.

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/impormaq';
+$config['base_url']	= 'http://localhost/getIt';
 
 /*
 |--------------------------------------------------------------------------
