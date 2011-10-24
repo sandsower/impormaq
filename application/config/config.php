856cf9fd4bbc008d5f4e425767bc3b0333fde475
Carlos Bolaños <garrisonx@gmail.com>
2011-10-24 07:40:47 -0500

    merge sanders

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/getIt';
+$config['base_url']	= 'http://localhost/impormaq';
 
 /*
 |--------------------------------------------------------------------------
